a6255a5b24047f4fe19fb38c2ac716bd116e8567
git4lhe <68907073+git4lhe@users.noreply.github.com>
2021-01-14 22:52:19 +0900

    Add boj 1436

---
+++ DP/평범한_배낭.py
@@ -0,0 +1,12 @@
+
+
+# N,K = map(int, input().split())
+# bag = {'weights':[], 'values':[]}
+#
+# for i in range(N):
+#     weight, value = map(int,input().split())
+#     bag['weights'].append(weight)
+#     bag['values'].append(value)
+#
+# w_list = [0] * N
+# v_list = [0] * N

06e3c32de0e9e739e263cc32997074019ea62a90
Julie,Haeun <68907073+git4lhe@users.noreply.github.com>
2021-02-01 15:56:26 +0900

    Solve problem

---
+++ Stack_and_Queue/주식가격.py
@@ -0,0 +1,16 @@
+def solution(prices):
+    answer = [1] * len(prices)
+    leng = len(prices)
+
+    for i in range(leng):
+        for j in range(i+1, leng):
+            if prices[i] > prices[j]:
+                break
+        answer[i] = j-i
+
+    return answer
+
+print("answer",solution([1, 2, 3, 2, 3]))
+
+
+#[4, 3, 1, 1, 0]

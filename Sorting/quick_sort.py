b303b2b99e8d09d47e7657295c90e4654be7d319
Julie,Haeun <68907073+git4lhe@users.noreply.github.com>
2021-02-25 16:27:03 +0900

    Add pythonic sorting code

--- Sorting/quick_sort.py
+++ Sorting/quick_sort.py
@@ -6,11 +6,13 @@ def quick_sort(arr, start, end):
     pivot = start
     left = start + 1
     right = end
+    p_value = arr[pivot]
 
+   #피봇 작은거-> <-큰것것
     while (left <= right):
 
-        # 피봇보다 더 큰값들을 찾아냄
-        # 피봇 작으면 pass
+    # 피봇보다 더 큰값들을 찾아냄
+    # 피봇 작으면 pass
         while (left <= right and arr[left] <= arr[pivot]):
             left += 1
 
@@ -20,8 +22,7 @@ def quick_sort(arr, start, end):
             right -= 1
 
         # left가 right보다 인덱스가 더 크다는것은
-        # 피봇보다 더 큰 값들이 많았다는 거임
-        if left > right:  # 엇갈렸다면
+        if left > right:  # 엇갈렸다면 left가 피봇보다 작은것 찾아냄
             arr[right], arr[pivot] = arr[pivot], arr[right]
 
         else:  # 엇갈리지 않았다면
@@ -29,8 +30,27 @@ def quick_sort(arr, start, end):
 
     quick_sort(arr, start, right - 1)
     quick_sort(arr, right + 1, end)
-
-
-arr = [5, 6, 15, 8, 9, 12, 9]
+import random
+import time
+arr = [random.randint(1,1000) for x in range(1000)]
+now = time.time()
 quick_sort(arr, 0, len(arr) - 1)
-print(arr)
+print(time.time() - now)
+
+def pythonic_quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[0]
+    tail = arr[1:]
+
+    left_side = [x for x in tail if x<= pivot]
+    right_side = [x for x in tail if x>= pivot]
+
+    return pythonic_quick_sort(left_side) + [pivot] + pythonic_quick_sort(right_side)
+
+arr = [random.randint(1,1000) for x in range(1000)]
+now = time.time()
+pythonic_quick_sort(arr)
+print(time.time() - now)
+print("finish")

5a3f26e2288b668c592a8b40c34e85e2b60c229f
Julie, Haeun Lee <68907073+git4lhe@users.noreply.github.com>
2021-04-26 23:29:51 +0900

    삼성 기출 20057번 문제풀이 마법사 상어와 토네이도

--- Samgsung/bj_20057.py
+++ Samgsung/bj_20057.py
@@ -1,96 +1,120 @@
-# 격자 가운데 부터 시작함
-# 토네이도가 x에서 y로 이동하면, y의 모든 모래가 비율과 α가 적혀있는 칸으로 이동한다.
-def spread(x, y,dir):
-    # sand = board[x][y]
-    locs.append([x,y,dir])
-    print(locs)
-    sum_up = 0
-    if dir == 0: # <-, ->
-        for i in range(len(left)):
-            nx = x + left[i][0]
-            ny = y + left[i][1]
-            sum_up += int(left[i][2] * board[x][y])
-            if 0 <= nx < N and 0 <= ny < N:
-                board[nx][ny] += int(left[i][2] * board[x][y])
-        if 0 <= x < N and 0 <= y-1 < N:
-            board[x][y-1] += (board[x][y] - sum_up)
+def spread_up(x,y):
+    dx = [-1, -1, 0, 0, 0, 0, 1, 1, 2]
+    dy = [-1, 1, -2, -1, 1, 2, -1, 1, 0]
+    dv = [0.01, 0.01, 0.02, 0.07, 0.07, 0.02, 0.1, 0.1, 0.05]
 
-    elif dir == 1:  # ↓
+    sum_value = 0
+    for idx, idy, idv in zip(dx, dy, dv):
+        nx = x - idx
+        ny = y - idy
+        sum_value += int(board[x][y] * idv)
+        if 0 <= nx < N and 0 <= ny < N:
+            board[nx][ny] += int(board[x][y] * idv)
 
+    if 0<= x-1 < N and 0<= y < N:
+        board[x - 1][y] += board[x][y] - sum_value
+    board[x][y] = 0
 
+def spread_down(x,y):
+    global board
 
-        for i in range(len(down)):
-            nx = x + down[i][0]
-            ny = y + down[i][1]
-            sum_up += int(down[i][2] * board[x][y])
-            if 0 <= nx < N and 0 <= ny < N:
-                board[nx][ny] += int(down[i][2] * board[x][y])
-        if 0 <= x+1 < N and 0 <= y < N:
-            board[x + 1][y] += (board[x][y] - sum_up)
+    dx = [-1, -1, 0, 0, 0, 0, 1, 1, 2]
+    dy = [-1, 1, -2, -1, 1, 2, -1, 1, 0]
+    dv = [0.01, 0.01, 0.02, 0.07, 0.07, 0.02, 0.1, 0.1, 0.05]
 
-    elif dir == 2: # ->
-        for i in range(len(left)):
-            nx = x - left[i][0]
-            ny = y - left[i][1]
-            sum_up += int(left[i][2] * board[x][y])
-            if 0 <= nx < N and 0 <= ny < N:
-                board[nx][ny] += int(left[i][2] * board[x][y])
-        if 0 <= x < N and 0 <= y+1 < N:
-            board[x][y+1] += (board[x][y] - sum_up)
+    sum_value = 0
+    for idx, idy, idv in zip(dx, dy, dv):
+        nx = x + idx
+        ny = y + idy
+        sum_value += int(board[x][y] * idv)
+        if 0 <= nx < N and 0 <= ny < N:
+            board[nx][ny] += int(board[x][y] * idv)
 
-    elif dir == 3: # ↑
-        for i in range(len(down)):
-            nx = x - down[i][0]
-            ny = y - down[i][1]
-            sum_up += int(down[i][2] * board[x][y])
-            if 0 <= nx < N and 0 <= ny < N:
-                board[nx][ny] += int(down[i][2] * board[x][y])
-        if 0 <= x-1 < N and 0 <= y < N:
-            board[x - 1][y] += (board[x][y] - sum_up)
+    if 0<= x+1 < N and 0<= y < N:
+        board[x + 1][y] += board[x][y] - sum_value
+    board[x][y] = 0
+
+def spread_left(x,y):
+    global board
+
+    dx = [-2, -1, -1, -1, 0, 1, 1, 1, 2]
+    dy = [0, -1, 0, 1, -2, -1, 0, 1, 0]
+    dv = [0.02, 0.1, 0.07, 0.01, 0.05, 0.1, 0.07, 0.01, 0.02]
+
+    sum_value = 0
+    for idx,idy,idv in zip(dx,dy,dv):
+        nx = x + idx
+        ny = y + idy
+        sum_value += int(board[x][y] * idv)
+        if 0<=nx<N and 0<=ny<N:
+            board[nx][ny] += int(board[x][y] * idv)
+
+    if 0<= x < N and 0<= y-1 < N:
+        board[x][y-1] += board[x][y] - sum_value
+    board[x][y] = 0
+
+def spread_right(x, y):
+    global board
+
+    dx = [-2, -1, -1, -1, 0, 1, 1, 1, 2]
+    dy = [0, -1, 0, 1, -2, -1, 0, 1, 0]
+    dv = [0.02, 0.1, 0.07, 0.01, 0.05, 0.1, 0.07, 0.01, 0.02]
+
+    sum_value = 0
+    for i,(idx, idy, idv) in enumerate(zip(dx, dy, dv)):
+        nx = x - idx
+        ny = y - idy
+        sum_value += int(board[x][y] * idv)
+        if 0 <= nx < N and 0 <= ny < N:
+            board[nx][ny] += int(board[x][y] * idv)
+
+    if 0<= x < N and 0<= y+1 < N:
+        board[x][y + 1] += board[x][y] - sum_value
+    board[x][y] = 0
+
+def spread(x,y,dir):
+    if dir == 0:
+        spread_left(x,y)
+
+    elif dir == 1:
+        spread_down(x,y)
+
+    elif dir == 2:
+        spread_right(x,y)
+
+    elif dir == 3:
+        spread_up(x,y)
 
 N = int(input())
-sum_value = 0
 board = []
+org_sand = 0
 for i in range(N):
     row = list(map(int,input().split()))
     board.append(row)
-    sum_value += sum(row)
+    org_sand += sum(row)
 
+x,y = N // 2, N // 2
 
+cnt = 1
 dx = [0,1,0,-1]
 dy = [-1,0,1,0]
-locs = []
-#
-left = [[0,-2, 0.05],[-1,-1,0.1],[-1,0,0.07],[-1,+1,0.01],[-2,0,0.02],
-         [+1,-1,0.1],[+1,0,0.07],[+1,+1,0.01],[+2,0,0.02]]
-# right = -left
+# < 아래
 
-down = [[-1,-1,0.01],[-1,+1,0.01], [0,+2,0.02],[0,-1,0.07],[0,-2,0.02],[0,+1,0.07],
-        [+1,-1,0.1], [+1,+1,0.1],[+2,0,0.05]]
-# up = -down
+stop = False
+while(1):
+    for dir in range(4):
+        if dir == 2:
+            cnt += 1
+        for size in range(cnt):
+            x = x + dx[dir]
+            y = y + dy[dir]
 
-x, y, dir = N // 2, N // 2, 0
-move = 1
+            spread(x,y,dir)
 
-while (0<=x<N and 0<=y<N):
-    x = x + dx[dir % 4] * move
-    y = y + dy[dir % 4] * move
-    if (0<=x<N and 0<=y<N):
-        spread(x,y,dir % 4)
-    board[x][y] = 0
-
-    dir += 1
-    x = x + dx[dir % 4] * move
-    y = y + dy[dir % 4] * move
-    if (0 <= x < N and 0 <= y < N):
-        spread(x,y, dir % 4)
-    board[x][y] = 0
-
-    move += 1
-    dir += 1
-
-
-answer = 0
-for i in range(N):
-    answer += sum(board[i])
-print(answer)
+            if x == 0 and y == 0:
+                v = 0
+                for i in range(N):
+                    v += sum(board[i])
+                print(org_sand - v)
+                exit()
+    cnt += 1
